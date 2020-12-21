a391690af3eedc180dc1d01ec9faa601571b18d1
ujasbatheja <71878150+ujasbatheja@users.noreply.github.com>
2020-12-21 18:09:56 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,35 @@
+class Player{
+    constructor(){
+        this.index=null;
+        this.distance=0;
+        this.name=null;
+    }
+    getPlayerCount(){
+        var playerCountRef = database.ref("playerCount");
+        playerCountRef.on("value",function(data){
+            playerCount = data.val();
+        })
+    }
+    updatePlayerCount(count){
+        database.ref("/").update({
+            playerCount: count
+        })
+    }
+    updatePlayerInfo(){
+        var playerIndex = "players/player"+this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance
+        })
+    }
+    static  getPlayerInfo(){
+        var playerInfoRef = database.ref("players");
+        playerInfoRef.on("value",function(data){
+            allPlayers = data.val();
+        })
+    }
+    static deletePlayerInfo(){
+        var playerInfoRef = database.ref("players");
+        playerInfoRef.remove();
+    }
+}
